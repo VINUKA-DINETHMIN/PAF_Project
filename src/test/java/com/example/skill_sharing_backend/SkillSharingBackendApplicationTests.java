dbddb54e4399add02d8913e3be507fed708cc6a1
VINUKA-DINETHMIN <IT22248626@my.sliit.lk>
2025-04-27 18:16:28 +0530

    create backend application java

---
+++ src/test/java/com/example/skill_sharing_backend/SkillSharingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill_sharing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSharingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
