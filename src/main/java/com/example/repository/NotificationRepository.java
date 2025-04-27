a50fdda068f88dc117950ba08ffd661c4b2a0b14
VINUKA-DINETHMIN <IT22248626@my.sliit.lk>
2025-04-27 18:29:45 +0530

    create notification repository

---
+++ src/main/java/com/example/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import com.example.model.Notification;
+import com.example.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Page<Notification> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
+    long countByUserAndIsReadFalse(User user);
+} 
