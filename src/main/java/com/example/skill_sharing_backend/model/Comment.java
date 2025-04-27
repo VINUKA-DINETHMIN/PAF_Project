95eb5c7a280b1842a754054ea2d8b1d72733cefa
Isuru <it22189462@my.sliit.lk>
2025-04-27 22:53:00 +0530

    Model Comment.java

---
+++ src/main/java/com/example/skill_sharing_backend/model/Comment.java
@@ -0,0 +1,28 @@
+package com.example.skill_sharing_backend.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+    private LocalDateTime createdAt;
+}
