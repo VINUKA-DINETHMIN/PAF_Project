dc71bee1ba2f471afaa338bbda3bd620bf0bf01a
Isuru <it22189462@my.sliit.lk>
2025-04-28 10:26:28 +0530

    CommentServiceImpl.java

---
+++ src/main/java/com/example/skill_sharing_backend/model/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.example.skill_sharing_backend.repository;
+
+
+
+import com.example.skill_sharing_backend.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
